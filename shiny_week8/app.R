66c2578ff759a426a88325946dcf5cc56018e964
Michael Hazboun <hazbo002@umn.edu>
2024-03-15 22:59:02 -0500

    I think mostly done, need to review and deploy

--- shiny_week8/app.R
+++ shiny_week8/app.R
@@ -8,26 +8,23 @@
 #
 
 library(shiny)
+library(tidyverse) #loaded tidyverse because I'll most probably be using it
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) #setworking directory to file that this thing lives in
 
-# Define UI for application that draws a histogram
 ui <- fluidPage(
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
+      titlePanel("shiny_week8"), #changed title to make it shiny_week8 on browser and app
 
-    # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+            radioButtons("genderbutton", "Would you like to see that data for:",choices = c("All","Male","Female"),"All"), # made a radio button that has a label of "would you like to..." and give the choices seen with a default of "All". I also gave it an ID of "genderbutton". I chose a button because I felt it would be easier for this to be a button over a drop down choice.
+            radioButtons("before", "Would you like to include participants that completed the assessment before July 1st, 2017?", c("Yes","No"),"Yes"),# made a radio button that has a label of "would you like to..." and give a yes or no with a default of "Yes". I also gave it an ID of "before". I chose a button because I felt it would be easier for this to be a button over a drop down choice.
+            selectInput("ErrorBand","What would you like to do with the error bands?",c("Display Error Band", "Suppress Error Band"),"Display Error Band") # made a selector that has a label of "what would you like to..." and give the choices seen with a default of "Display Error Band". I also gave it an ID of "ErrorBand". I chose a selector this time because I thought three buttons wouldn't look great, plus this one had longer choices, so a selector felt nicer than a radio button
+            
         ),
 
-        # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot")
+           plotOutput("Plot") #changed the id to plot
         )
     )
 )
@@ -35,15 +32,22 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    output$Plot <- renderPlot({ #changed the output$somethingPlot to output$Plot, to match new ID
+        
+      shinydata <- readRDS("./data.rds")
+      
+      if(input$genderbutton != "All"){ shinydata <- shinydata%>% #made a dataset that changes based on selected gender (used an if not equal to All, then filter based on input value)
+        filter(gender==input$genderbutton)
+      }
+      
+      if(input$before=="No"){shinydata <- shinydata%>%
+        filter(timeEnd>=ymd("2017-07-01"))} #made the data change depending on the answer to the second question, also used ymd to determine date because the format I was using before wasn't working and dates are annoying and lubridate makes my life easier.
 
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
+        shinydata %>% #made the same graph as last time, but will edit it slightly.
+          ggplot(aes(x=mq1_6,y=mq8_10))+
+          geom_point() +
+          geom_smooth( method = "lm",color="purple",se= ifelse(input$ErrorBand=="Display Error Band",TRUE,FALSE))+ #added an extra ifelse statement here to turn on or off the error bands depending on the response (give a TRUE or FALSE output depending on input)
+          labs(x= "Mean of Q1 through Q6", y= "Mean of Q8 through Q10")
     })
 }
 
